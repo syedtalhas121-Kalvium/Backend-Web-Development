b900bacb743aa5fefc81a6987645f1a0f08424a8
syedtalhas121-Kalvium <syed.talha.s.121@kalvium.community>
2026-08-30 12:16:34 +0000

    Implement request timing middleware

--- Module 2/Express Middleware Architecture/middleware/timing.js
+++ Module 2/Express Middleware Architecture/middleware/timing.js
@@ -1,16 +1,17 @@
 /**
  * timing middleware  [mount GLOBALLY in app.js]
  *
- * TODO: export a middleware function (req, res, next) that logs how long the
- *       request took, in milliseconds.
- *   - record a start time at the top (Date.now())
- *   - on the response 'finish' event, log Date.now() - start
- *   - include req.id if it is set
- *   - call next() immediately so the request continues
- *
- * Example line:  [a3f9c1e2] POST /posts took 14ms
+ * Measures the time from middleware entry until the response finishes.
  */
 
 module.exports = function timing(req, res, next) {
-  // TODO: capture start, register res.on('finish', ...) to log elapsed ms, then next().
+  const start = Date.now();
+
+  res.on('finish', () => {
+    const elapsed = Date.now() - start;
+    const prefix = req.id ? `[${req.id.substring(0, 8)}] ` : '';
+    console.log(`${prefix}${req.method} ${req.path} took ${elapsed}ms`);
+  });
+
+  next();
 };
