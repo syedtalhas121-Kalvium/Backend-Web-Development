078c008c5a8b5d7e61c40adc8e3b538fe91eb273
syedtalhas121-Kalvium <syed.talha.s.121@kalvium.community>
2026-09-08 03:36:53 +0000

    refactor: remove controller error handling boilerplate

--- Module 2/Supporting Layers/controllers/articlesController.js
+++ Module 2/Supporting Layers/controllers/articlesController.js
@@ -1,36 +1,16 @@
-/**
- * Articles controller — STARTER.
- *
- * Each handler uses a repetitive try/catch to forward errors. Once you create
- * utils/asyncHandler.js you can wrap these in the route file and drop the
- * try/catch boilerplate here (the handlers become plain async functions).
- */
+const service = require('../services/articlesService');
 
-const service = require('./../services/articlesService');
-
-exports.list = async (req, res, next) => {
-  try {
-    const articles = await service.getAll();
-    res.json({ data: articles });
-  } catch (err) {
-    next(err);
-  }
+exports.list = async (req, res) => {
+  const articles = await service.getAll();
+  res.json({ data: articles });
 };
 
-exports.create = async (req, res, next) => {
-  try {
-    const article = await service.create(req.body);
-    res.status(201).json({ data: article });
-  } catch (err) {
-    next(err);
-  }
+exports.create = async (req, res) => {
+  const article = await service.create(req.body);
+  res.status(201).json({ data: article });
 };
 
-exports.update = async (req, res, next) => {
-  try {
-    const article = await service.update(Number(req.params.id), req.body);
-    res.json({ data: article });
-  } catch (err) {
-    next(err);
-  }
+exports.update = async (req, res) => {
+  const article = await service.update(Number(req.params.id), req.body);
+  res.json({ data: article });
 };
